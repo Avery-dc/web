828cc09b04db3202830a42de9673398ac55a696c
a3510377 <a102009102009@gmail.com>
2022-01-28 22:02:43 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { config } from "dotenv";
 config();
-
+console.log(process.env);
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE,
