bf7efada1df37e2c2c42374fee9ae2b1654b97f8
a3510377 <a102009102009@gmail.com>
2022-02-02 21:30:23 +0800

    Update i18n.ts

--- src/directives/i18n.ts
+++ src/directives/i18n.ts
@@ -2,26 +2,20 @@ import { Directive, DirectiveBinding, computed } from "vue";
 
 import { useStore } from "@/store";
 import { Modules } from "@/store";
-import { classify } from "@/utils";
 
 const store = useStore();
 
 export default <Directive>{
-  created(a, b, vnode) {
-    console.log();
-    let options = vnode.dirs?.[0].instance?.$options;
-    console.log(options);
-  },
   mounted(el: HTMLElement, binding: DirectiveBinding<string>) {
     let { arg, value } = binding;
-    let { key, defaultTxt: txt } = <{ key?: string; defaultTxt: string }>(
-      /^\/(?<key>([a-zA-Z0-9])*)\/(?<defaultTxt>)/gm.exec(value)?.groups
-    ) || { key: void 0, defaultTxt: "" };
+    let { key, txt } = <{ key: string; txt: string }>(
+      /(^\/(?<key>([a-zA-Z0-9])*)\/)?(?<txt>.*)/gm.exec(value)?.groups
+    ) || { key: void 0, txt: "" };
 
     let language = computed(() => store.state[Modules.I18N].language);
     let data = computed(() => store.state[Modules.I18N].data);
 
-    if (key) txt = data.value[language.value]?.[key] || txt;
+    key && (txt = data.value[language.value]?.[`${arg}-${key}`] || txt);
 
     el.innerText = txt;
   },
