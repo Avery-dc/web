38cb82319095073c7c898745c756e6264812fa3e
avery <a102009102009@gmail.com>
2022-02-03 12:37:03 +0000

    buildedit f809605b86c7936969a1093ca3a1e0c0a4f075d1

---
+++ assets/discord.021ec67b.js
@@ -0,0 +1,1 @@
+import{d as e,o}from"./vendor.297796ad.js";const c=e({setup(n){return o(()=>{window.opener&&(window.opener.window.dispatchEvent(new CustomEvent("get_dc_code",{detail:{code:new URLSearchParams(window.location.search).get("code")}})),window.close())}),(t,d)=>null}});export{c as default};
