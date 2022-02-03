0f3a091cf357d572422530740a2c3211c32d655c
avery <a102009102009@gmail.com>
2022-02-03 08:29:20 +0000

    buildadd footer 7ae06a55ee964cb3298f98ddda53083b22f98d69

---
+++ assets/discord.dcacd98b.js
@@ -0,0 +1,1 @@
+import{d as e,o}from"./vendor.9f3e402b.js";const r=e({setup(n){return o(()=>{window.opener&&(window.opener.window.dispatchEvent(new CustomEvent("get_dc_code",{detail:{code:new URLSearchParams(window.location.search).get("code")}})),window.close())}),(t,d)=>null}});export{r as default};
